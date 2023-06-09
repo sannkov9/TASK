2c2324a9d472f345cea8dae4c1999cb60b4dd9f6
Ilyasan <sannkov9@gmail.com>
2023-06-09 18:17:19 +0700

    версия 1.21

---
+++ task033/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите прграмму которая определяет
+//  присутсвует ли данное число в массиве
+// 4; массив (6,7,19,345,3) -> нет
+// 3; массив (6,7,19,345,3) -> да
+
+int [] CreateArryaRndInt(int size, int min, int max)
+{
+    int[] arr = new int [size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max +1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+bool CheckTheNumber(int num, int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == num) return true; 
+    }
+    return false;
+}
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArryaRndInt(5, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+
+bool checkTheNumber = CheckTheNumber(number, array);
+Console.WriteLine();
+Console.WriteLine(checkTheNumber ? "Your number exists in the array" : "Your number doesn't exist in the array");
