cddd0915bcf8230b5a225733e1e804621619ac51
Ilyasan <sannkov9@gmail.com>
2023-06-09 18:24:47 +0700

    версия 1.22

---
+++ task032/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу замена элементов массива
+// Положительные элементы замените на соответсвующие
+// отрицательные и наооборот.
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int [size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+} 
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+void InverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+int[] array = CreateArrayRndInt(5, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+InverseArray(array);
+Console.WriteLine();
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
